2009fafba61113725269a56626982217b3d3d104
deepk2891 <deepk2891@gmail.com>
2024-07-02 17:51:38 +0530

    filteredList by using useState

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 import RestaurantCard from "./RestaurantCard"
 import { Grid, Button, Container, Box } from "@mui/material"
 import resList from "../utils/mockData"
 
 const Body = () => {
-	const listOfRestaurants = resList
+	const [listOfRestaurants, setListOfRestaurants] = useState(resList)
 
 	return (
-		<Container sx={{ pt: 5 }}>
-			<Box className="Search" sx={{ px: 2 }}>
-				<Button variant="contained" color="primary">
+		<Container sx={{ pt: 2 }}>
+			<Box className="Search" sx={{ p: 2 }}>
+				<Button
+					variant="contained"
+					color="primary"
+					onClick={() => {
+						const filteredList = listOfRestaurants.filter((res) => res.data.avgRating > 4)
+						setListOfRestaurants(filteredList)
+					}}
+				>
 					Top Rated Restaurant
 				</Button>
 			</Box>
