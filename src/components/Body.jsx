54cfe7ccf637e138816c5a323b33e26a56e211ed
deepk2891 <deepk2891@gmail.com>
2024-07-02 20:28:10 +0530

    🚧 checking deployment fetch api

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,20 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import RestaurantCard from "./RestaurantCard"
 import { Grid, Button, Container, Box } from "@mui/material"
 import resList from "../utils/mockData"
 
 const Body = () => {
 	const [listOfRestaurants, setListOfRestaurants] = useState(resList)
+
+	useEffect(() => {
+		fetchData()
+	}, [])
+
+	const fetchData = async () => {
+		const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING")
+		const json = await data.json()
+		console.log(json)
+	}
 
 	return (
 		<Container sx={{ pt: 2 }}>
