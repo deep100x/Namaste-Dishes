e8192197f9d39ad0e733b49b4358afff594f9d3e
deepk2891 <deepk2891@gmail.com>
2024-07-09 20:29:25 +0530

    Search filter

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react"
 import RestaurantCard from "./RestaurantCard"
-import { Grid, Button, Container, Box } from "@mui/material"
+import { Grid, Button, Container, Box, TextField } from "@mui/material"
 import resList from "../utils/mockData"
 import Shimmer from "./Shimmer"
 
 const Body = () => {
 	const [listOfRestaurants, setListOfRestaurants] = useState([])
+	const [searchText, setSearchText] = useState("")
+	const [filteredRestaurants, setFilteredRestaurants] = useState([])
 
 	const fetchData = async () => {
 		try {
@@ -16,6 +18,7 @@ const Body = () => {
 			let resList = json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants || json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
 			console.log("json", resList)
 			setListOfRestaurants(resList || [])
+			setFilteredRestaurants(resList || [])
 			console.log("listOfRestaurants", listOfRestaurants)
 		} catch (error) {
 			console.log(error)
@@ -26,6 +29,11 @@ const Body = () => {
 		fetchData()
 	}, [])
 
+	useEffect(() => {
+		const filteredList = listOfRestaurants.filter((res) => res?.info?.name.toLowerCase().includes(searchText.toLowerCase()))
+		setFilteredRestaurants(filteredList)
+	}, [searchText, listOfRestaurants])
+
 	// if (listOfRestaurants.length === 0) {
 	// 	return <Shimmer showHeader={true} />
 	// }
@@ -33,21 +41,22 @@ const Body = () => {
 	return listOfRestaurants.length === 0 ? (
 		<Shimmer showHeader showCards />
 	) : (
-		<Container sx={{ pt: 2 }}>
-			<Box className="Search" sx={{ p: 2 }}>
+		<Container sx={{ pt: 2, minHeight: "100dvh" }}>
+			<Box className="Search" sx={{ py: 2, display: "flex", alignItems: "center", gap: 2 }}>
+				<TextField label="Search" id="outlined-size-small" size="small" onChange={(e) => setSearchText(e.target.value)} />
 				<Button
-					variant="contained"
+					variant="outlined"
 					color="primary"
 					onClick={() => {
 						const filteredList = listOfRestaurants.filter((res) => res?.info?.avgRating > 4)
-						setListOfRestaurants(filteredList)
+						setFilteredRestaurants(filteredList)
 					}}
 				>
-					Top Rated Restaurant
+					Rating 4.0+
 				</Button>
 			</Box>
 			<Grid container spacing={2}>
-				{listOfRestaurants.map((restaurant) => (
+				{filteredRestaurants.map((restaurant) => (
 					<RestaurantCard key={restaurant?.info?.id} resData={restaurant} />
 				))}
 			</Grid>
